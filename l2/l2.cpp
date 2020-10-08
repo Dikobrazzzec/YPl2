ab0451f892f4538dd345e2d18df3374612095ff0
Dikobrazzzec <zasxcd1307@mail.ru>
2020-10-08 19:28:47 +0300

    pre final

--- l2/l2.cpp
+++ l2/l2.cpp
@@ -18,7 +18,23 @@ int main()
 	HCRYPTPROV b;
 	//CryptAcquireContext(&b, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT);
 	HCRYPTHASH hashdeskr;													//насколько безлопасно использовать один дескриптор на 2 функции?
+	HCRYPTHASH duplhashptr1;
+	HCRYPTHASH duplhashptr2;
+	HCRYPTHASH duplhashptr3;
+	DWORD bytevalue;
+	BYTE duplhasharray1[100];
+	BYTE duplhasharray2[100];
+	BYTE duplhasharray3[100];
 	CryptCreateHash(a,CALG_MD5,0,0, &hashdeskr);
 	CryptHashData(hashdeskr, value, QuanBytes, NULL);
+	CryptDuplicateHash(hashdeskr, NULL, NULL, &duplhashptr1);
+	CryptDuplicateHash(hashdeskr, NULL, NULL, &duplhashptr2);
+	CryptDuplicateHash(hashdeskr, NULL, NULL, &duplhashptr3);
+	CryptGetHashParam(hashdeskr, HP_ALGID, duplhasharray1, &bytevalue,NULL);
+	cout << duplhasharray1 << endl;
+	CryptGetHashParam(duplhashptr1,HP_HASHSIZE , duplhasharray2, &bytevalue, NULL);
+	cout << duplhasharray2 << endl;
+	CryptGetHashParam(duplhashptr1,HP_HASHVAL , duplhasharray3, &bytevalue, NULL);
+	cout << duplhasharray3 << endl;
 	return 0;
 }
